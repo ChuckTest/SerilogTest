9089f0aec8bd925f663b02e11739c675a0da9304
Chuck Lu <chuck.lu@qq.com>
2020-07-09 17:04:58 +0800

    handle FailureCallback

--- SerilogTest/Program.cs
+++ SerilogTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace SerilogTest
 {
@@ -11,9 +12,17 @@ namespace SerilogTest
             {
                 Guid guid = Guid.NewGuid();
 
-                Log.Logger = new LoggerConfiguration().ReadFrom.AppSettings()
-                    .CreateLogger();
+                var loggerConfiguration = new LoggerConfiguration();
+                loggerConfiguration.ReadFrom.AppSettings();
+                Log.Logger = loggerConfiguration.CreateLogger();
 
+                var loggerConfiguration2 = new LoggerConfiguration()
+                    .WriteTo.Logger(Log.Logger)
+                    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions()
+                    {
+                        FailureCallback =e => Console.WriteLine("Unable to submit event " + e.MessageTemplate)
+                    });
+                Log.Logger = loggerConfiguration2.CreateLogger();
                 for (int i = 0; i < 1000; i++)
                 {
                     Log.Information($"{guid}, {i}, this is a test log {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff zzz}.");
