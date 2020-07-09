129f088ca5a016a5f16a58f4c4a46303e4e709c7
Chuck Lu <chuck.lu@qq.com>
2020-07-09 11:06:22 +0800

    try to use elastic search with serilog

--- SerilogTest/Program.cs
+++ SerilogTest/Program.cs
@@ -7,27 +7,31 @@ namespace SerilogTest
     {
         static void Main(string[] args)
         {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .WriteTo.Console()
-                .WriteTo.File("logs\\myapp.txt", rollingInterval: RollingInterval.Day)
-                .CreateLogger();
-
-            Log.Information("Hello, world!");
-
-            int a = 10, b = 0;
             try
             {
-                Log.Debug("Dividing {A} by {B}", a, b);
-                Console.WriteLine(a / b);
+                Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Debug()
+                    .CreateLogger();
+
+                Log.Information("Hello, world!");
+
+                int a = 10, b = 0;
+                try
+                {
+                    Log.Debug("Dividing {A} by {B}", a, b);
+                    Console.WriteLine(a / b);
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(ex, "Something went wrong");
+                }
+
+                Log.CloseAndFlush();
             }
             catch (Exception ex)
             {
-                Log.Error(ex, "Something went wrong");
+                Console.WriteLine(ex);
             }
-
-            Log.CloseAndFlush();
-            Console.ReadKey();
         }
     }
 }
