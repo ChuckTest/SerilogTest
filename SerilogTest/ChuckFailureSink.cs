d26336a7fa61a2d826b3812f273eb63a479e4f3b
Chuck Lu <chuck.lu@qq.com>
2020-07-10 16:27:39 +0800

    implement ILogEventSink as ChuckFailureSink

---
+++ SerilogTest/ChuckFailureSink.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace SerilogTest
+{
+    class ChuckFailureSink : ILogEventSink
+    {
+        void ILogEventSink.Emit(LogEvent logEvent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
