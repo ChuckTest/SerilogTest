4eb4bb4ff36825ae887eb1ffbfaeb17cfbaf1da7
Chuck Lu <chuck.lu@qq.com>
2020-07-13 14:55:26 +0800

    test Serilog.Sinks.File

---
+++ SerilogTest2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+using Serilog.Debugging;
+
+namespace SerilogTest2
+{
+    class Program
+    {
+        static string GetConfigPath()
+        {
+            const string testsConfig = "serilog.config";
+            if (File.Exists(testsConfig))
+                return Path.GetFullPath(testsConfig);
+            var basePath = AppDomain.CurrentDomain.BaseDirectory;
+            return Path.GetFullPath(Path.Combine(basePath, testsConfig));
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                SelfLog.Enable(SelfLogHandler);
+
+                var path = GetConfigPath();
+                Guid guid = Guid.NewGuid();
+
+                var loggerConfiguration = new LoggerConfiguration();
+                loggerConfiguration = loggerConfiguration.ReadFrom.AppSettings(filePath: path);
+                var logger = loggerConfiguration.CreateLogger();
+                Log.Logger = logger;
+                for (int i = 0; i < 1000; i++)
+                {
+                    Log.Information($"{guid}, {i}, this is a test log {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff zzz}.");
+                }
+                Log.Information($"{guid}, this is a test log {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff zzz}.");
+                Log.Error($"{guid}, this is a test log {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff zzz}.");
+                Log.CloseAndFlush();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        static readonly SerilogEventLog lisaEventLog = new SerilogEventLog();
+        static void SelfLogHandler(string log)
+        {
+            lisaEventLog.WriteEntry(log, EventLogEntryType.Error);
+        }
+    }
+}
---
+++ SerilogTest2/SerilogEventLog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SerilogTest2
+{
+    internal class SerilogEventLog
+    {
+        private readonly string _logName = @"Serilog";
+
+        public string LogName => _logName;
+
+        public SerilogEventLog()
+        {
+        }
+
+        public SerilogEventLog(string logName)
+        {
+            _logName = logName;
+        }
+
+        public void WriteEntry(string error, EventLogEntryType type)
+        {
+            var sourceName = AppDomain.CurrentDomain.FriendlyName;
+            if (!EventLog.SourceExists(sourceName))
+            {
+                EventLog.CreateEventSource(sourceName, _logName);
+            }
+
+            using (EventLog eventLog = new EventLog(_logName))
+            {
+                eventLog.Source = sourceName;
+                var message = $"{AppDomain.CurrentDomain.BaseDirectory}{Environment.NewLine}{error}";
+                eventLog.WriteEntry(message, type);
+            }
+        }
+    }
+}
