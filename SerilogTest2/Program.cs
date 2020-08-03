04df7a30f92e8b15df3d293d6f4d952183dfefd6
Chuck Lu <chuck.lu@qq.com>
2020-08-03 18:04:13 +0800

    write three log to monitor the output easily

--- SerilogTest2/Program.cs
+++ SerilogTest2/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Text;
 using System.Threading;
 using Microsoft.Extensions.Configuration;
 using Serilog;
@@ -12,42 +11,6 @@ namespace SerilogTest2
 {
     class Program
     {
-        static string GetConfigPath()
-        {
-            const string componentName = "serilog";
-            const string fileExtension = "config";
-            var fileName = $"{componentName}.{fileExtension}";
-            var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.AppendLine();
-            var configFile =
-                new FileInfo(Path.Combine(baseDirectory,
-                    AppDomain.CurrentDomain.FriendlyName + $".{fileName}"));
-            if (!configFile.Exists)
-            {
-                stringBuilder.AppendLine(configFile.FullName);
-                configFile =
-                    new FileInfo(Path.Combine(baseDirectory, $"{fileName}"));
-            }
-            if (!configFile.Exists)
-            {
-                stringBuilder.AppendLine(configFile.FullName);
-                configFile =
-                    new FileInfo(Path.Combine(baseDirectory, "bin",
-                        $"{fileName}"));
-            }
-            if (!configFile.Exists)
-            {
-                serilogEventLog.WriteEntry($"Can not find the configuration in following path {stringBuilder}", EventLogEntryType.Warning);
-                return string.Empty;
-            }
-            else
-            {
-                return configFile.FullName;
-            }
-        }
-
         static void Main(string[] args)
         {
             try
@@ -64,15 +27,9 @@ namespace SerilogTest2
                 Log.Logger = logger;
 
                 Guid guid = Guid.NewGuid();
-                for (int i = 0; i < 30; i++)
-                {
-                    Log.Information($"{guid}, {i}, this is a test log {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff zzz}.");
-                    Thread.Sleep(500);
-                }
-
-                Log.Information($"{guid}, this is a test log {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff zzz}.");
-                Log.Error($"{guid}, this is a test log {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff zzz}.");
-                Log.Debug($"{guid}, this is a test log {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff zzz}.");
+                Log.Information($"{guid}, this is a information log {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff zzz}.");
+                Log.Error($"{guid}, this is an error log {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff zzz}.");
+                Log.Debug($"{guid}, this is a debug log {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff zzz}.");
                 Log.CloseAndFlush();
             }
             catch (Exception ex)
